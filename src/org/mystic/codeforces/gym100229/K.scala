5dc952de81ae39bd439a59f838133663b0faddf9
Konstantin Perikov <konstantin.perikov@kohls.com>
2013-10-02 11:36:52 +0400

    Add K solution

--- src/org/mystic/codeforces/gym100229/K.scala
+++ src/org/mystic/codeforces/gym100229/K.scala
@@ -22,19 +22,27 @@ object K {
   def solve = {
     val n = nextInt
     val arr = new Array[Int](n + 2)
+    val dp = new Array[Int](n + 2)
     arr(0) = 0
+    dp(0) = 0
     arr(n + 1) = 0
     for (i <- 1 to n) {
       arr(i) = nextInt
     }
     val k = nextInt
+    for (i <- 1 to n + 1) {
+      var max = Int.MinValue
+      for (j <- Math.max(0, i - k) to i - 1) {
+        max = Math.max(max, dp(j))
+      }
+      dp(i) = arr(i) + max
+    }
+    out.println(dp(n + 1))
   }
 
   def main(args: Array[String]): Unit = {
-    br = new BufferedReader(new InputStreamReader(System.in))
-    out = new PrintWriter(new BufferedOutputStream(System.out))
-    //    br = new BufferedReader(new InputStreamReader(new FileInputStream("ladder.in")))
-    //    out = new PrintWriter(new BufferedOutputStream(new FileOutputStream("ladder.out")))
+    br = new BufferedReader(new InputStreamReader(new FileInputStream("ladder.in")))
+    out = new PrintWriter(new BufferedOutputStream(new FileOutputStream("ladder.out")))
     solve
     out.close
   }
