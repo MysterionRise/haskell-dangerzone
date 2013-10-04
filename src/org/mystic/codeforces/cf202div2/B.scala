626793893fdd716e91c4f1035847ffb469d1d555
Konstantin Perikov <konstantin.perikov@kohls.com>
2013-10-04 14:35:09 +0400

    Add solutions

--- src/org/mystic/codeforces/cf202div2/B.scala
+++ src/org/mystic/codeforces/cf202div2/B.scala
@@ -28,17 +28,44 @@ object B {
     var min: Int = Integer.MAX_VALUE
     var pos: Int = -1
     for (i <- 0 until 9) {
-      if (num(i) < min) {
+      if (num(i) <= min) {
         min = num(i)
         pos = i
       }
     }
-    val numberOfDigits: Int = v / min
+    val len: Int = v / min
+    if (len == 0) {
+      out.println(-1);
+    } else {
+      val res = StringBuilder.newBuilder
+      for (i <- 0 until len) {
+        res.append(pos + 1)
+      }
+      val ch = res.toArray
+      var left = v - len * min
+      var i = 0
+      while (i < len && left > 0) {
+        val count = num(ch(i).toInt - 48 - 1)
+        var posJ = -1
+        for (j <- 0 until 9) {
+          if (count + left >= num(j)) {
+            posJ = j
+          }
+        }
+        if (posJ != -1) {
+          ch(i) = (posJ + 1 + 48).toChar
+          left = left + count - num(posJ)
+        }
+        i = i + 1
+      }
+      ch.foreach(out.print)
+    }
+
   }
 
   def main(args: Array[String]): Unit = {
-    br = new BufferedReader(new InputStreamReader(new FileInputStream("absum.in")))
-    out = new PrintWriter(new BufferedOutputStream(new FileOutputStream("absum.out")))
+    br = new BufferedReader(new InputStreamReader(System.in))
+    out = new PrintWriter(new BufferedOutputStream(System.out))
     solve
     out.close
   }
