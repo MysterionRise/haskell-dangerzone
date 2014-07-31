c59eee839d6930eb98630c4b9bfe1ed201ff1ddf
Konstantin Perikov <kperikov@griddynamics.com>
2014-07-31 17:47:13 +0400

    Add B

--- src/main/java/org/mystic/codeforces/cf10/B.scala
+++ src/main/java/org/mystic/codeforces/cf10/B.scala
@@ -29,22 +29,60 @@ object B {
     val n = nextInt
     val k = nextInt
     val cinema = new Array[Array[Int]](k)
+    val xc = Math.ceil(k / 2).toInt
     for (i <- 0 until k) {
       cinema(i) = new Array[Int](k)
     }
     for (i <- 0 until k) {
       for (j <- 0 until k) {
-        cinema(i)(j) = func(k, i, j)
+        cinema(i)(j) = func(xc, i, j)
       }
     }
-    for (i <- 0 until n) {
+    for (t <- 0 until n) {
       val m = nextInt
-      // TODO
+      var sum = Int.MaxValue
+      var start = 0
+      var row = 0
+      for (i <- 0 until k) {
+        var interSum = 0
+        for (j <- 0 until m) {
+          interSum += cinema(i)(j)
+        }
+        if (interSum < sum) {
+          sum = interSum
+          row = i
+          start = 0
+        }
+        for (j <- m until k) {
+          interSum += cinema(i)(j)
+          interSum -= cinema(i)(j - m)
+          if (interSum < sum) {
+            sum = interSum
+            row = i
+            start = j - m + 1
+          }
+        }
+      }
+      if (sum < 100000) {
+//        out.println(sum)
+        out.println((row + 1) + " " + (start + 1) + " " + (start + m))
+        for (j <- start until start + m) {
+          cinema(row)(j) = 100000
+        }
+      } else {
+        out.println(-1)
+      }
+//      for (i <- 0 until k) {
+//        for (j <- 0 until k) {
+//          out.print(cinema(i)(j) + " ")
+//        }
+//        out.println
+//      }
     }
   }
 
-  def func(k: Int, x: Int, y: Int) = {
-    Math.abs(x - k / 2) + Math.abs(y - k / 2)
+  def func(xc: Int, x: Int, y: Int) = {
+    Math.abs(x - xc) + Math.abs(y - xc)
   }
 
   def main(args: Array[String]): Unit = {
