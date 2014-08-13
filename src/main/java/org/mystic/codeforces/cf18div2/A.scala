3eb649fbd192d6f6eae56d5b942550155dc376c4
Konstantin Perikov <kperikov@griddynamics.com>
2014-08-13 15:50:52 +0400

    Add sols

---
+++ src/main/java/org/mystic/codeforces/cf18div2/A.scala
@@ -0,0 +1,120 @@
+package org.mystic.codeforces.cf18div2
+
+import java.util._
+import java.lang._
+import java.io._
+
+object A {
+
+  var out: PrintWriter = null
+  var br: BufferedReader = null
+  var st: StringTokenizer = null
+
+  def next: String = {
+    while (st == null || !st.hasMoreTokens) {
+      st = new StringTokenizer(br.readLine)
+    }
+    return st.nextToken
+  }
+
+  def nextInt: Int = {
+    return Integer.parseInt(next)
+  }
+
+  def nextLong: Long = {
+    return Long.parseLong(next)
+  }
+
+  def sqDist(x1: Int, y1: Int, x2: Int, y2: Int): Int = {
+    return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)
+  }
+
+  def dist(x1: Int, y1: Int, x2: Int, y2: Int): Double = {
+    return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2))
+  }
+
+  def isSquareTriangle(x1: Int, y1: Int, x2: Int, y2: Int, x3: Int, y3: Int): Boolean = {
+    val ab2 = sqDist(x1, y1, x2, y2)
+    val bc2 = sqDist(x2, y2, x3, y3)
+    val ac2 = sqDist(x1, y1, x3, y3)
+    val ab = dist(x1, y1, x2, y2)
+    val bc = dist(x3, y3, x2, y2)
+    val ac = dist(x1, y1, x3, y3)
+    if (ac < ab + bc && ab < ac + bc && bc < ac + ab) {
+      if (ab2 + bc2 == ac2 || ab2 + ac2 == bc2 || bc2 + ac2 == ab2) {
+        return true
+      }
+    }
+    return false
+  }
+
+  def solve: Int = {
+    val x1 = nextInt
+    val y1 = nextInt
+    val x2 = nextInt
+    val y2 = nextInt
+    val x3 = nextInt
+    val y3 = nextInt
+    if (isSquareTriangle(x1, y1, x2, y2, x3, y3)) {
+      out.println("RIGHT")
+      return 1
+    }
+    if (isSquareTriangle(x1 + 1, y1, x2, y2, x3, y3)) {
+      out.println("ALMOST")
+      return 1
+    }
+    if (isSquareTriangle(x1 - 1, y1, x2, y2, x3, y3)) {
+      out.println("ALMOST")
+      return 1
+    }
+    if (isSquareTriangle(x1, y1 + 1, x2, y2, x3, y3)) {
+      out.println("ALMOST")
+      return 1
+    }
+    if (isSquareTriangle(x1, y1 - 1, x2, y2, x3, y3)) {
+      out.println("ALMOST")
+      return 1
+    }
+    if (isSquareTriangle(x1, y1, x2 + 1, y2, x3, y3)) {
+      out.println("ALMOST")
+      return 1
+    }
+    if (isSquareTriangle(x1, y1, x2 - 1, y2, x3, y3)) {
+      out.println("ALMOST")
+      return 1
+    }
+    if (isSquareTriangle(x1, y1, x2, y2 + 1, x3, y3)) {
+      out.println("ALMOST")
+      return 1
+    }
+    if (isSquareTriangle(x1, y1, x2, y2 - 1, x3, y3)) {
+      out.println("ALMOST")
+      return 1
+    }
+    if (isSquareTriangle(x1, y1, x2, y2, x3 + 1, y3)) {
+      out.println("ALMOST")
+      return 1
+    }
+    if (isSquareTriangle(x1, y1, x2, y2, x3 - 1, y3)) {
+      out.println("ALMOST")
+      return 1
+    }
+    if (isSquareTriangle(x1, y1, x2, y2, x3, y3 + 1)) {
+      out.println("ALMOST")
+      return 1
+    }
+    if (isSquareTriangle(x1, y1, x2, y2, x3, y3 - 1)) {
+      out.println("ALMOST")
+      return 1
+    }
+    out.println("NEITHER")
+    return 1
+  }
+
+  def main(args: Array[String]): Unit = {
+    br = new BufferedReader(new InputStreamReader(System.in))
+    out = new PrintWriter(new BufferedOutputStream(System.out))
+    solve
+    out.close
+  }
+}
---
+++ src/main/java/org/mystic/codeforces/cf18div2/B.scala
@@ -0,0 +1,45 @@
+package org.mystic.codeforces.cf18div2
+
+import java.util
+import java.util._
+import java.lang._
+import java.io._
+
+object B {
+
+  var out: PrintWriter = null
+  var br: BufferedReader = null
+  var st: StringTokenizer = null
+
+  def next: String = {
+    while (st == null || !st.hasMoreTokens) {
+      st = new StringTokenizer(br.readLine)
+    }
+    return st.nextToken
+  }
+
+  def nextInt: Int = {
+    return Integer.parseInt(next)
+  }
+
+  def nextLong: Long = {
+    return Long.parseLong(next)
+  }
+
+  def solve: Int = {
+    val n = nextInt
+    val d = nextInt
+    val m = nextInt
+    val l = nextInt
+    var pos = 0
+    out.println(pos)
+    return 1
+  }
+
+  def main(args: Array[String]): Unit = {
+    br = new BufferedReader(new InputStreamReader(System.in))
+    out = new PrintWriter(new BufferedOutputStream(System.out))
+    solve
+    out.close
+  }
+}
