cefba354ec46d27588349daabf1eadcbc0390a9a
Konstantin Perikov <kperikov@griddynamics.com>
2014-08-29 12:03:37 +0400

    Add B

--- src/main/java/org/mystic/codeforces/gym100083/B.scala
+++ src/main/java/org/mystic/codeforces/gym100083/B.scala
@@ -25,68 +25,56 @@ object B {
     return java.lang.Long.parseLong(next)
   }
 
-
   def solve: Int = {
     val n = nextInt
+    val graph = new Array[util.ArrayList[Int]](n)
+    (0 until n).foreach(i => graph(i) = new util.ArrayList[Int]())
     val m = nextInt
-    val g = new Array[util.ArrayList[Int]](n)
-    (0 until n).foreach(i => g(i) = new util.ArrayList[Int]())
-    (0 until m).foreach(_ => g(nextInt - 1).add(nextInt - 1))
-    val graph = new Array[Array[Int]](n)
-    (0 until n).foreach {
+    (0 until m).foreach(_ => graph(nextInt - 1).add(nextInt - 1))
+    val g = new Array[Array[Int]](n)
+    (0 until n).foreach({
       i =>
-        graph(i) = new Array[Int](g(i).size())
-        (0 until graph(i).length).foreach(j => graph(i)(j) = g(i).get(j))
-    }
+        g(i) = new Array[Int](graph(i).size())
+        (0 until g(i).length).foreach(j => g(i)(j) = graph(i).get(j))
+    })
 
     object Helper {
 
       val color = new Array[Char](n)
       val path = new util.ArrayList[Int]()
-      var end = -1
 
-      def isACycle(v: Int): Boolean = {
+      def findCycle(v: Int): Boolean = {
+        if (color(v) == 'b') {
+          return false
+        }
         if (color(v) == 'g') {
-          end = v
+          path.add(v)
           return true
         }
         color(v) = 'g'
-        for (i <- 0 until graph(v).length) {
-          val j: Int = graph(v)(i)
-          if (color(j) != 'b') {
-            if (isACycle(j)) {
-              path.add(j)
-              color(j) = 'b'
+        (0 until g(v).length).foreach({
+          i =>
+            if (findCycle(g(v)(i))) {
+              path.add(v)
+              color(g(v)(i)) = 'b'
               return true
             }
-          }
-        }
+        })
         color(v) = 'b'
         return false
       }
     }
-    Arrays.fill(Helper.color, 'w')
-    var flag = false
-    (0 until n).takeWhile(_ => !flag).foreach(i => flag |= Helper.isACycle(i))
-    if (flag) {
-      val len: Int = Helper.path.size()
-      var cnt = 0
-      for (i <- 0 until len) {
-        if (Helper.path.get(i) == Helper.end) {
-          cnt += 1
+    (0 until n).foreach({
+      i =>
+        if (Helper.findCycle(i)) {
+          val end = Helper.path.get(0)
+          out.println("YES")
+          val len = Helper.path.size()
+          (len - 1 to 1 by -1).dropWhile(i => Helper.path.get(i) != end).foreach(i => out.print((Helper.path.get(i) + 1) + " "))
+          return 1
         }
-      }
-      out.println("YES")
-      if (cnt == 1) {
-        (len - 1 to 0 by -1).foreach(i => out.print((Helper.path.get(i) + 1) + " "))
-      } else {
-        val cleaned = (len - 1 to 0 by -1).dropWhile(i => Helper.path.get(i) != Helper.end)
-        out.print((Helper.path.get(cleaned.head) + 1) + " ")
-        cleaned.drop(1).takeWhile(i => Helper.path.get(i) != Helper.end).foreach(i => out.print((Helper.path.get(i) + 1) + " "))
-      }
-    } else {
-      out.println("NO")
-    }
+    })
+    out.println("NO")
     return 1
   }
 
