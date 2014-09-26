ac3463943086ebb063ce49d6c5f664ce392d64b4
Konstantin Perikov <kperikov@griddynamics.com>
2014-09-26 16:00:49 +0400

    Add C, upd version, still WA 14

--- src/main/java/org/mystic/codeforces/gym100230/C.scala
+++ src/main/java/org/mystic/codeforces/gym100230/C.scala
@@ -2,7 +2,6 @@ package org.mystic.codeforces.gym100230
 
 import java.util._
 import java.io._
-import java.util
 
 object C {
 
@@ -36,7 +35,6 @@ object C {
       val d = new Array[Long](n)
 
       val INF: Long = Long.MaxValue
-      val INF1: Long = INF - 1
 
       def findShortestPathsFrom(v: Int) = {
         Arrays.fill(d, INF)
@@ -47,45 +45,46 @@ object C {
               j =>
                 if (d(edges(j)._1) < INF) {
                   d(edges(j)._2) = Math.min(d(edges(j)._2), d(edges(j)._1) + edges(j)._3)
+                  d(edges(j)._2) = Math.max(-INF, d(edges(j)._2))
                 }
             })
         })
         val e = new Array[Long](n)
         (0 until n).foreach(i => e(i) = d(i))
-        (0 until m).foreach({
-          j =>
-            if (d(edges(j)._1) < INF) {
-              d(edges(j)._2) = Math.min(d(edges(j)._2), d(edges(j)._1) + edges(j)._3)
-            }
+        (0 until n).foreach({
+          i =>
+            (0 until m).foreach({
+              j =>
+                if (d(edges(j)._1) < INF) {
+                  d(edges(j)._2) = Math.min(d(edges(j)._2), d(edges(j)._1) + edges(j)._3)
+                  d(edges(j)._2) = Math.max(-INF, d(edges(j)._2))
+                }
+            })
         })
         (0 until n).foreach({
           i =>
-            if(d(i) < e(i)) {
-              d(i) = INF1
+            if (d(i) < e(i)) {
+              out.println("-")
+            } else {
+              if (d(i) == INF) {
+                out.println("*")
+              } else {
+                out.println(d(i))
+              }
             }
         })
 
       }
-
-      def format(ans: Long): String = ans match {
-        case INF => "*"
-        case INF1 => "-"
-        case _ => ans.toString
-      }
     }
     Helper.findShortestPathsFrom(s)
-    (0 until n).foreach({
-      i =>
-        out.println(Helper.format(Helper.d(i)))
-    })
     return 1
   }
 
   def main(args: Array[String]): Unit = {
-//    br = new BufferedReader(new InputStreamReader(System.in))
-//        out = new PrintWriter(new BufferedOutputStream(System.out))
-        br = new BufferedReader(new InputStreamReader(new FileInputStream("path.in")))
-        out = new PrintWriter(new BufferedOutputStream(new FileOutputStream("path.out")))
+//            br = new BufferedReader(new InputStreamReader(System.in))
+//                    out = new PrintWriter(new BufferedOutputStream(System.out))
+    br = new BufferedReader(new InputStreamReader(new FileInputStream("path.in")))
+    out = new PrintWriter(new BufferedOutputStream(new FileOutputStream("path.out")))
     solve
     out.close
   }
