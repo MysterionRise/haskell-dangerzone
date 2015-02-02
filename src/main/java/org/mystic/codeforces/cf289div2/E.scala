08e0154184a61580c69d5d30c2dc50cdf245e461
Konstantin Perikov <kperikov@griddynamics.com>
2015-02-02 04:13:26 +0100

    Add E

--- src/main/java/org/mystic/codeforces/cf289div2/E.scala
+++ src/main/java/org/mystic/codeforces/cf289div2/E.scala
@@ -1,7 +1,7 @@
 package org.mystic.codeforces.cf289div2
 
 import java.io._
-import java.util.{Locale, StringTokenizer}
+import java.util.StringTokenizer
 
 object E {
 
@@ -28,18 +28,23 @@ object E {
     val chars = next.toCharArray
     val n = chars.length
     val vowels = Set('A', 'Y', 'E', 'U', 'I', 'O')
-    var ans = 0.0f
+    var ans = 0.0d
+    val sum1 = new Array[Double](n + 1)
+    for (i <- 1 until n + 1) {
+      sum1(i) = sum1(i - 1) + (1.0d / i)
+    }
+    val sum2 = new Array[Double](n + 1)
+    sum2(n) = 1.0d / n
+    for (i <- n - 1 to 0 by - 1) {
+      sum2(i) = sum2(i + 1) + (1.0d * n - i + 1) / i
+    }
     (1 to n).foreach(i => {
       if (vowels.contains(chars(i - 1))) {
         val min = Math.min(i, n - i + 1)
-        var k = 1
-        for (j <- 1 to min) {
-          ans += (1.0f * j) / k
-          k += 1
-        }
-        for (j <- min + 1 to n) {
-          ans += 
-        }
+        val max = Math.max(i, n - i + 1)
+        ans += min - 1
+        ans += min * (sum1(max - 1) - sum1(min - 1))
+        ans += sum2(max)
       }
     })
     out.println(ans)
