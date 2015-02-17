a89263213e1482b41b90525222a8e946972954af
Konstantin Perikov <kperikov@griddynamics.com>
2015-02-17 22:59:50 +0100

    Add template D

---
+++ src/main/java/org/mystic/codeforces/cf292div2/D.scala
@@ -0,0 +1,43 @@
+package org.mystic.codeforces.cf292div2
+
+import java.io._
+import java.util._
+
+object D {
+
+  var out: PrintWriter = null
+  var br: BufferedReader = null
+  var st: StringTokenizer = null
+
+  def next: String = {
+    while (st == null || !st.hasMoreTokens) {
+      st = new StringTokenizer(br.readLine)
+    }
+    return st.nextToken
+  }
+
+  def nextInt: Int = {
+    return Integer.parseInt(next)
+  }
+
+  def nextLong: Long = {
+    return java.lang.Long.parseLong(next)
+  }
+
+  def solve: Int = {
+    val n = nextInt
+    val m = nextInt
+    val field = new Array[Array[Char]]
+    for (i <- 0 until n) {
+      field(i) = next.toCharArray
+    }
+    return 1
+  }
+
+  def main(args: Array[String]): Unit = {
+    br = new BufferedReader(new InputStreamReader(System.in))
+    out = new PrintWriter(new BufferedOutputStream(System.out))
+    solve
+    out.close
+  }
+}
