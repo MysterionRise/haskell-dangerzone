9fb062ce814ea68c00eef81e04baf7b8953387be
Konstantin Perikov <MysterionRise@users.noreply.github.com>
2015-03-20 18:05:03 +0100

    Create B.scala

---
+++ src/main/java/org/mystic/codeforces/vkcup2015/B.scala
@@ -0,0 +1,107 @@
+package org.mystic.codeforces.vkcup2015
+
+import java.io._
+import java.util.StringTokenizer
+import java.util.TreeMap
+import scala.collection.mutable
+
+object B {
+
+  var out: PrintWriter = null
+  var br: BufferedReader = null
+  var st: StringTokenizer = null
+
+  def next: String = {
+    while (st == null || !st.hasMoreTokens) {
+      st = new StringTokenizer(br.readLine)
+    }
+    return st.nextToken
+  }
+
+  def nextInt: Int = {
+    return Integer.parseInt(next)
+  }
+
+  def nextLong: Long = {
+    return java.lang.Long.parseLong(next)
+  }
+
+  class MultiHashSet[T <% Comparable[T]] {
+    val map = new mutable.HashMap[T, Int]()
+
+    def count(x: T): Int = {
+      return map.getOrElse(x, 0)
+    }
+
+    def add(x: T): Unit = map.put(x, count(x) + 1)
+
+    def remove(x: T): Boolean = {
+      val prev = count(x)
+      if (prev == 0)
+        return false
+      if (prev == 1) {
+        map.remove(x)
+      } else {
+        map.put(x, prev - 1)
+      }
+      return true
+    }
+  }
+
+  class MultiTreeSet[T <% Comparable[T]] {
+    val map = new TreeMap[T, Int]()
+
+    def count(x: T): Int = {
+      val res = map.get(x)
+      if (res == null)
+        return 0
+      return res
+    }
+
+    def add(x: T): Unit = map.put(x, count(x) + 1)
+
+    def first(): T = return map.firstKey()
+
+    def last(): T = return map.lastKey()
+
+    def remove(x: T): Boolean = {
+      val prev = count(x)
+      if (prev == 0)
+        return false
+      if (prev == 1) {
+        map.remove(x)
+      } else {
+        map.put(x, prev - 1)
+      }
+      return true
+    }
+  }
+
+  def solve: Int = {
+    val n = nextInt
+    val friends = new Array[(Int, Int)](n)
+    var W = 0
+    val H = new MultiTreeSet[Int]()
+    for (i <- 0 until n) {
+      friends(i) = (nextInt, nextInt)
+      W += friends(i)._1
+      H.add(friends(i)._2)
+    }
+    for (i <- 0 until n) {
+      val minW = W - friends(i)._1
+      H.remove(friends(i)._2)
+      var minH = 0
+      minH = H.last
+      H.add(friends(i)._2)
+      out.print(minW.toLong * minH.toLong + " ")
+    }
+    return 1
+  }
+
+  def main(args: Array[String]): Unit = {
+    br = new BufferedReader(new InputStreamReader(System.in))
+    out = new PrintWriter(new BufferedOutputStream(System.out))
+    solve
+    out.close
+  }
+}
