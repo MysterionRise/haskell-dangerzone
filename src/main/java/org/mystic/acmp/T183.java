64ff07f3c892089b1d5e8771bbff3257fdcf65d8
Konstantin Perikov <MysterionRise@users.noreply.github.com>
2015-04-23 16:43:03 +0200

    Create T183.java

---
+++ src/main/java/org/mystic/acmp/T183.java
@@ -0,0 +1,47 @@
+package org.mystic.acmp;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class T183 implements Runnable {
+
+    PrintWriter out;
+    BufferedReader br;
+    StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        new Thread(new T183()).start();
+    }
+
+    public String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine());
+        }
+        return st.nextToken();
+    }
+
+    public int nextInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    public long nextLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    public void run() {
+        try {
+//            br = new BufferedReader(new FileReader("input.txt"));
+//            out = new PrintWriter(new FileWriter("output.txt"));
+            br = new BufferedReader(new InputStreamReader(System.in));
+            out = new PrintWriter(new BufferedOutputStream(System.out));
+            solve();
+            out.close();
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    public void solve() throws IOException {
+        //TODO
+    }
+}
