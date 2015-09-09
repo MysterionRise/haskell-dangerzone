243aed0da782fcf5acd193b0f65b3a2f9ed0043f
Konstantin Perikov <MysterionRise@users.noreply.github.com>
2015-09-09 16:20:18 +0200

    Create C466.scala

---
+++ src/main/java/org/mystic/codeforces/ladder6/C466.scala
@@ -0,0 +1,72 @@
+package org.mystic.codeforces.ladder6
+
+import java.io._
+import java.util.StringTokenizer
+
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+
+
+object C466 {
+
+  var result = true
+  var out: PrintWriter = null
+  var br: BufferedReader = null
+  var st: StringTokenizer = null
+
+  def main(args: Array[String]): Unit = {
+    br = new BufferedReader(new InputStreamReader(System.in))
+    out = new PrintWriter(new BufferedOutputStream(System.out))
+    solve
+    out.close
+  }
+
+  def next: String = {
+    while (st == null || !st.hasMoreTokens) {
+      st = new StringTokenizer(br.readLine)
+    }
+    return st.nextToken
+  }
+
+  def nextInt: Int = return Integer.parseInt(next)
+
+  def nextLong: Long = return java.lang.Long.parseLong(next)
+
+  def nextDouble: Double = return java.lang.Double.parseDouble(next)
+
+  def solve: Int = {
+    val n = nextInt
+    val a = new Array[Int](n)
+    val prefixSum = new Array[Int](n)
+    val map = new mutable.HashMap[Int, ArrayBuffer[Int]]()
+    a(0) = nextInt
+    prefixSum(0) = a(0)
+    for (i <- 1 until n) {
+      a(i) = nextInt
+      prefixSum(i) = a(i) + prefixSum(i - 1)
+    }
+    for (i <- 0 until n) {
+      if (map.contains(prefixSum(i))) {
+        map.get(prefixSum(i)).get.append(i)
+      } else {
+        val ab = new ArrayBuffer[Int]()
+        ab.append(i)
+        map.put(prefixSum(i), ab)
+      }
+    }
+    var ans = 0L
+    for (pos <- 1 until n - 1) {
+      val sum = prefixSum(pos - 1)
+      val left = prefixSum(n - 1) - prefixSum(pos - 1)
+      if (left / 2 == sum && left % 2 == 0) {
+        val positions = map.getOrElse(2 * sum, ArrayBuffer[Int]())
+        positions.foreach(p => {
+          if (p >= pos && prefixSum(n - 1) - prefixSum(p) == sum && p != n - 1)
+            ans += 1
+        })
+      }
+    }
+    println(ans)
+    return 0
+  }
+}
