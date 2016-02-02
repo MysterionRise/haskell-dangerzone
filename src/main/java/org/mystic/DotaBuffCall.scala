d79942d24e8d1227be2fd722f7db0500e4f46274
Konstantin Perikov <kperikov@griddynamics.com>
2016-02-02 22:32:32 +0100

    add new calls and proper xhtml parsing

--- src/main/java/org/mystic/DotaBuffCall.scala
+++ src/main/java/org/mystic/DotaBuffCall.scala
@@ -7,6 +7,8 @@ import org.apache.http.client.methods.HttpGet
 import org.apache.http.client.{HttpResponseException, ResponseHandler}
 import org.apache.http.impl.client.{BasicResponseHandler, DefaultHttpClient}
 
+import scala.xml.parsing.XhtmlParser
+
 object DotaBuffCall {
 
   def main(args: Array[String]): Unit = {
@@ -23,8 +25,22 @@ object DotaBuffCall {
         val responseHandler: ResponseHandler[String] = new BasicResponseHandler
         val responseBody = client.execute(get, responseHandler)
         if (responseBody.length > 0) {
+          val html = XhtmlParser(scala.io.Source.fromString(responseBody))
+
           val out = new PrintWriter(name.substring(name.lastIndexOf("/") + 1))
-          out.println(responseBody)
+          out.println((html \ "html" \ "body" \ "div[1]" \ "div[7]" \ "div[2]" \ "div[1]" \ "div[2]").text)
+          out.println((html \ "html" \ "body" \ "div[1]" \ "div[7]" \ "div[3]" \ "div[1]").text)
+//          out.println((xml \\ "header-content-secondary").text)
+          //          xml \\ "html/body/div[1]/div[7]/div[2]/div[1]/div[2]"
+
+          // header
+          ///html/body/div[1]/div[7]/div[2]/div[1]/div[2]
+
+          // body
+          ///html/body/div[1]/div[7]/div[3]/div[1]
+          //          val start = responseBody.indexOf("<div class=\"match-show\">")
+          //          val end = responseBody.indexOf("<div id=\"kunkka-bottom-leaderboard\"")
+          //          out.println(responseBody.substring(start, end))
           out.flush()
           out.close()
           //          val patternBody: Pattern = Pattern.compile("\"_score\":([^]])+]")
