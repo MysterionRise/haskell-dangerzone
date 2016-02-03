eeb5d2138375494b5d7cc34faaf25fb378e5f119
Konstantin Perikov <kperikov@griddynamics.com>
2016-02-03 23:18:07 +0100

    migrate to jsoup

--- src/main/java/org/mystic/DotaBuffCall.scala
+++ src/main/java/org/mystic/DotaBuffCall.scala
@@ -1,58 +1,22 @@
 package org.mystic
 
-import java.io.PrintWriter
 import java.util.Scanner
 
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.client.{HttpResponseException, ResponseHandler}
-import org.apache.http.impl.client.{BasicResponseHandler, DefaultHttpClient}
+import org.jsoup.Jsoup
 
-import scala.xml.XML
 
 object DotaBuffCall {
 
   def main(args: Array[String]): Unit = {
     val in = new Scanner(System.in)
     val n = in.nextInt()
-
-    var client = new DefaultHttpClient
     in.nextLine()
     for (i <- 0 until n) {
-      try {
-        client = new DefaultHttpClient
-        val name = in.nextLine()
-        val get: HttpGet = new HttpGet(name)
-        val responseHandler: ResponseHandler[String] = new BasicResponseHandler
-        val responseBody = client.execute(get, responseHandler)
-        if (responseBody.length > 0) {
-          val html = XML.loadString(responseBody.substring(responseBody.indexOf("<body>"), responseBody.indexOf("</body>") + 7))
-
-          val out = new PrintWriter(name.substring(name.lastIndexOf("/") + 1))
-          out.println((html \ "html" \ "body" \ "div[1]" \ "div[7]" \ "div[2]" \ "div[1]" \ "div[2]").text)
-          out.println((html \ "html" \ "body" \ "div[1]" \ "div[7]" \ "div[3]" \ "div[1]").text)
-//          out.println((xml \\ "header-content-secondary").text)
-          //          xml \\ "html/body/div[1]/div[7]/div[2]/div[1]/div[2]"
-
-          // header
-          ///html/body/div[1]/div[7]/div[2]/div[1]/div[2]
-
-          // body
-          ///html/body/div[1]/div[7]/div[3]/div[1]
-          //          val start = responseBody.indexOf("<div class=\"match-show\">")
-          //          val end = responseBody.indexOf("<div id=\"kunkka-bottom-leaderboard\"")
-          //          out.println(responseBody.substring(start, end))
-          out.flush()
-          out.close()
-        }
-      }
-      catch {
-        case e: HttpResponseException => {
-          val message: String = e.getStatusCode + ", " + e.getMessage
-          System.out.println("Error " + message)
-        }
-      } finally {
-        client.getConnectionManager.shutdown
-      }
+      val name = in.nextLine()
+      val doc = Jsoup.connect(name)
+        .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
+        .get()
+      println(doc.getAllElements)
     }
   }
 }
