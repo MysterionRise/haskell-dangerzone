167b77732fc96c1356a4731ec5cd7095e6a91aa0
Konstantin Perikov <MysterionRise@users.noreply.github.com>
2016-02-24 14:11:41 +0100

    Update DotaBuffCall.scala

--- src/main/java/org/mystic/DotaBuffCall.scala
+++ src/main/java/org/mystic/DotaBuffCall.scala
@@ -3,6 +3,7 @@ package org.mystic
 import java.io.PrintWriter
 import java.net.{PasswordAuthentication, Authenticator}
 
+import org.joda.time.DateTime
 import org.joda.time.format.{DateTimeFormatterBuilder, DateTimeFormat}
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
@@ -14,10 +15,21 @@ import scala.collection.mutable.ArrayBuffer
 
 object DotaBuffCall {
 
-  val userNames = List("LeCassette", "Paul", "Grace", "Ice-CreaM^", "Zlodey", "JK", "Klydd", "P11", "Gwaed Oer", "improve", "RALFGTS", "LilAngryBoy")
   //"238193250" - out of scope, strange bug with matches
-  val userIDs = List("112427189", "116894024", "92253911", "224417226", "120301254", "165818364", "133208812", "42344936", "141246748",
-    "98365520", "118794347", "117746168", "248823035")
+  val users = Map(
+    "112427189" -> "LilAngryBoy",
+    "116894024" -> "LeCassette",
+    "92253911" -> "Grace",
+    "224417226" -> "Paul",
+    "120301254" -> "Cableman",
+    "165818364" -> "JK",
+    "133208812" -> "Klydd",
+    "42344936" -> "improve",
+    "141246748" -> "Ice-CreaM^",
+    "98365520" -> "P11",
+    "118794347" -> "Gwaed Oer",
+    "117746168" -> "RALFGTS",
+    "248823035" -> "Zlodey")
 
   val wins = new MultiHashSet[String]
   val losses = new MultiHashSet[String]
@@ -70,7 +82,7 @@ object DotaBuffCall {
   def printGameMode(gameMode: String) = gameMode.split(" ").map(mode => mode.charAt(0)).mkString
 
   def addWinsAndLosses(ourTeam: Element, win: Boolean) = {
-    userNames.filter(name => ourTeam.text.toLowerCase.contains(name.toLowerCase)).foreach(name =>
+    users.values.filter(name => ourTeam.text.toLowerCase.contains(name.toLowerCase)).foreach(name =>
       if (win) wins.add(name) else losses.add(name))
   }
 
@@ -105,7 +117,7 @@ object DotaBuffCall {
 
   def main(args: Array[String]): Unit = {
     setupProxy
-    val allMatches = userIDs.map(getAllMatches)
+    val allMatches = users.keySet.map(getAllMatches).toList
     val combinedMatches = new mutable.HashSet[String]
     for (i <- 0 until allMatches.size) {
       for (j <- i + 1 until allMatches.size) {
@@ -113,6 +125,7 @@ object DotaBuffCall {
       }
     }
     println(combinedMatches.size)
+    combinedMatches.foreach(name => println(s"http://www.dotabuff.com$name"))
     val n = combinedMatches.size
     val out = new PrintWriter("advanced-stats.txt")
     combinedMatches.foreach(name => {
@@ -144,15 +157,20 @@ object DotaBuffCall {
       val region = children.get(3 + ind).child(0).text()
       val parsers = Array(
         DateTimeFormat.forPattern("hh:mm:ss").getParser(),
-        DateTimeFormat.forPattern("mm:ss").getParser()
+        DateTimeFormat.forPattern("mm:ss").getParser(),
+        DateTimeFormat.forPattern("yyyy-mm-dd").getParser()
       )
       val formatter = new DateTimeFormatterBuilder().append(null, parsers).toFormatter()
       var duration = 0
+      var date: DateTime = null
       try {
         duration = (formatter.parseLocalTime(children.get(4 + ind).child(0).text()).getMillisOfDay / 1000)
+        date = formatter.parseDateTime(children.get(5 + ind).child(0).text())
+        //todo get date of match
       } catch {
         case e: Exception => {
           println(children.get(4).child(0).text())
+          println(children.get(5).child(0).text())
           println(name)
         }
       }
@@ -161,7 +179,7 @@ object DotaBuffCall {
       val radiantResults: Element = teamsResults.first.children().get(0)
       val direResults: Element = teamsResults.first.children().get(1)
       var ourTeam = ""
-      if (!userNames.filter(name => radiantResults.text.toLowerCase.contains(name.toLowerCase)).isEmpty) {
+      if (!users.values.filter(name => radiantResults.text.toLowerCase.contains(name.toLowerCase)).isEmpty) {
         ourTeam = "radiant"
       } else {
         ourTeam = "dire"
@@ -173,9 +191,9 @@ object DotaBuffCall {
         addBansAndPicks(findOurTeam(radiantBans, direBans, ourTeam))
       }
 
-      val numberOfPlayers = userNames.filter(name => radiantResults.text.toLowerCase.contains(name.toLowerCase)).size + userNames.filter(name => direResults.text.toLowerCase.contains(name.toLowerCase)).size
+      val numberOfPlayers = users.values.filter(name => radiantResults.text.toLowerCase.contains(name.toLowerCase)).size + users.values.filter(name => direResults.text.toLowerCase.contains(name.toLowerCase)).size
       addWinsAndLosses(findOurTeam(radiantResults, direResults, ourTeam), winOrLoss(ourTeam, win))
-      out.println(s"${printWin(ourTeam, win)}\t${printGameMode(gameMode)}\t$lobbyType\t$skillBracket\t$numberOfPlayers\t$duration\t$region")
+      out.println(s"${printWin(ourTeam, win)}\t${printGameMode(gameMode)}\t$lobbyType\t$skillBracket\t$numberOfPlayers\t$duration\t$region\t${date.toString("dd-mm-yyyy")}")
       out.flush()
       val teamAbilityBuilds = doc.select("div[class*=match-ability-builds")
     })
@@ -189,7 +207,6 @@ object DotaBuffCall {
     out.close()
   }
 
-  // if you need proxy for getting connection
   def setupProxy: String = {
     val authUser = "-"
     val authPassword = "-"
