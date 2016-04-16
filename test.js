1f8ee4c440b9d6ef02093c8aa1eb567b67819a14
Konstantin Perikov <MysterionRise@users.noreply.github.com>
2016-04-16 22:07:39 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,12 @@
 (function() {
-  var elems = document.getElementsByTagName("a"),
+var elems = document.getElementsByTagName("div"),
    i;
 for (i in elems) {
-   if (elems[i].href.indexOf("/rajonchik37") > 0) {
-      elems[i].innerHTML = "Лягуха";
+   if (elems[i].className == "wrapped") {
+      var innerDiv = elems[i].getElementsByTagName("div");
+      if (innerDiv[0].getElementsByTagName("a")[0].href.indexOf("/rajonchik37") > 0) {
+         innerDiv[1].innerHTML = "***********************";
+      }
    }
 }
 })();
