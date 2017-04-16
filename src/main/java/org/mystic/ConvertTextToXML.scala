90ab1da797d094cf4292b77ab50be86663700b4c
Konstantin Perikov <pericov.ko@gmail.com>
2017-04-16 20:50:06 +0300

    update solr tester

---
+++ src/main/java/org/mystic/ConvertTextToXML.scala
@@ -0,0 +1,76 @@
+package org.mystic
+
+import java.io.{File, PrintWriter}
+import java.util.Scanner
+
+import org.apache.solr.common.SolrInputDocument
+
+
+object ConvertTextToXML {
+
+  import org.apache.solr.client.solrj.SolrClient
+  import org.apache.solr.client.solrj.impl.HttpSolrClient
+
+  val urlString = "http://192.168.0.88:8983/solr/wiki"
+  val solr: SolrClient = new HttpSolrClient(urlString)
+
+  def createXMLFile(title: String, text: StringBuilder) = {
+//    val out = new PrintWriter(s"D:\\data\\${title.
+//      replaceAllLiterally(" ", "_").
+//      replaceAllLiterally("/", "_").
+//      replaceAllLiterally("?", "_").
+//      replaceAllLiterally("'", "_").
+//      replaceAllLiterally("*", "_").
+//      replaceAllLiterally("\"", "_").
+//      replaceAllLiterally(".", "_").
+//      replaceAllLiterally(",", "_").substring(0, 31)}.xml")
+    val doc = new SolrInputDocument()
+    doc.addField("title", title)
+    doc.addField("text", text.toString())
+    solr.add(doc)
+    //solr.commit()
+//    out.print("<add><doc>")
+//    out.print(s"""<field name="title">$title</field>""")
+//    out.print(s"""<field name="text">${text.toString().trim}</field>""")
+//    out.print("</doc></add>")
+//    out.flush
+//    out.close()
+  }
+
+  //000009
+  def convertToNDigits(i: Int, n: Int): String = {
+    val len = String.valueOf(i).length
+    val number = new StringBuilder
+    for (_ <- 0 until n - len) {
+      number.append("0")
+    }
+    number.append(String.valueOf(i))
+    number.toString()
+  }
+
+  def main(args: Array[String]) {
+    solr.deleteByQuery("*:*")
+    solr.commit()
+    for (i <- 0 until 500) {
+      val in = new Scanner(new File(s"D:\\wiki\\20140615-wiki-en_${convertToNDigits(i, 6)}.txt"))
+      val text = new StringBuilder
+      var title = ""
+      while (in.hasNextLine) {
+        val line = in.nextLine()
+        if (line.startsWith("[[")) {
+          if (title.length > 3) {
+            createXMLFile(title, text)
+            text.clear()
+          }
+          title = line.replaceAllLiterally("[", "").replaceAllLiterally("]", "")
+        } else if (line.startsWith("==")) {
+          // nothing
+        } else {
+          text.append(line)
+        }
+      }
+      solr.commit()
+    }
+    solr.close()
+  }
+}
--- src/main/java/org/mystic/SolrTester.scala
+++ src/main/java/org/mystic/SolrTester.scala
@@ -5,16 +5,15 @@ import java.io.PrintWriter
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics
 import org.apache.solr.client.solrj.{SolrClient, SolrQuery}
 import org.apache.solr.client.solrj.impl.HttpSolrClient
-
-import scala.collection.mutable.ListBuffer
 import scala.util.Random
-
 
 object SolrTester {
 
-  val TEST_SIZE = 100000
+  val TEST_SIZE = 1001
 
   val WORD_SIZE = 3
+
+  val INDEX_V = 2
 
   val urlString1 = "http://192.168.0.88:8983/solr/wiki"
   val solr1: SolrClient = new HttpSolrClient(urlString1)
@@ -37,24 +36,44 @@ object SolrTester {
 
 
   def main(args: Array[String]) {
+    val out = new PrintWriter(s"$INDEX_V-test-$TEST_SIZE-$WORD_SIZE.txt")
     val wildcardStats = new DescriptiveStatistics()
     val saStats = new DescriptiveStatistics()
-    for (_ <- 0 until TEST_SIZE) {
+    for (i <- 0 until TEST_SIZE) {
+      if (i % (TEST_SIZE / 10) == 0) {
+        println((10 * i) / (TEST_SIZE / 10) + "%")
+      }
       val word = generateWord(letters, WORD_SIZE)
-//      println(word)
       val query = s"*${word}*"
       wildcardStats.addValue(runWildcardQuery(query))
       saStats.addValue(runSuffixArrayQuery(query))
     }
+//    for (i <- 'a' to 'z')
+//      for (j <- 'a' to 'z')
+//        for (k <- 'a' to 'z') {
+//          val query = s"*$i$j$k*"
+//          val wild = runWildcardQuery(query)
+//          val sa = runSuffixArrayQuery(query)
+//          if (sa > wild) {
+//            out.println(s"$query $wild $sa")
+//            out.flush()
+//          }
+//          wildcardStats.addValue(wild)
+//          saStats.addValue(sa)
+//        }
+
     // calculate statistics
-    val out = new PrintWriter(s"test-$TEST_SIZE-$WORD_SIZE.txt")
+    out.println("name wildcard sa")
     out.println(s"mean ${wildcardStats.getMean} ${saStats.getMean}")
+    out.println(s"max ${wildcardStats.getMax} ${saStats.getMax}")
+    out.println(s"min ${wildcardStats.getMin} ${saStats.getMin}")
     out.println(s"percentile 50 ${wildcardStats.getPercentile(0.5d)} ${saStats.getPercentile(0.5d)}")
     out.println(s"percentile 60 ${wildcardStats.getPercentile(0.6d)} ${saStats.getPercentile(0.6d)}")
     out.println(s"percentile 70 ${wildcardStats.getPercentile(0.7d)} ${saStats.getPercentile(0.7d)}")
     out.println(s"percentile 80 ${wildcardStats.getPercentile(0.8d)} ${saStats.getPercentile(0.8d)}")
     out.println(s"percentile 90 ${wildcardStats.getPercentile(0.9d)} ${saStats.getPercentile(0.9d)}")
     out.println(s"percentile 95 ${wildcardStats.getPercentile(0.95d)} ${saStats.getPercentile(0.95d)}")
+    out.println(s"percentile 99 ${wildcardStats.getPercentile(0.99d)} ${saStats.getPercentile(0.99d)}")
     out.close()
     solr1.close()
     solr2.close()
