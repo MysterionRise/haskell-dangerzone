ebfc7c6c5680b920d2da208d46bd628839bb5b4b
Konstantin Perikov <MysterionRise@users.noreply.github.com>
2019-09-04 10:38:09 +0100

    Create TwoSum.java

---
+++ src/main/java/org/mystic/custom/leetcode/TwoSum.java
@@ -0,0 +1,13 @@
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        final int n = nums.length;
+        final Map<Integer, Integer> ind = new HashMap<>();
+        for (int i = 0; i < n; ++i) {
+            if (ind.containsKey(target - nums[i])) {
+                return new int[]{i, ind.get(target - nums[i])};
+            }
+            ind.put(nums[i], i);
+        }
+        return null;
+    }
+}
