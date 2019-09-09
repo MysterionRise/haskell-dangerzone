9f5a0c446c4158e0043cd54bfb9fc61e54dd97d7
Konstantin Perikov <MysterionRise@users.noreply.github.com>
2019-09-09 12:41:54 +0100

    Update AddTwoNumbers.java

--- src/main/java/org/mystic/custom/leetcode/AddTwoNumbers.java
+++ src/main/java/org/mystic/custom/leetcode/AddTwoNumbers.java
@@ -6,6 +6,9 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+
+import java.math.*;
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         StringBuilder sb1 = new StringBuilder();
@@ -18,8 +21,8 @@ class Solution {
             sb2.append(l2.val);
             l2 = l2.next;
         } while (l2 != null);
-        final int res = Long.parseLong(sb1.reverse().toString()) + Long.parseLong(sb2.reverse().toString());
-        char[] r = Long.toString(res).toCharArray();
+        final BigInteger res = new BigInteger(sb1.reverse().toString()).add(new BigInteger(sb2.reverse().toString()));
+        char[] r = res.toString().toCharArray();
         final ListNode result = new ListNode(r[r.length - 1] - '0');
         ListNode curr = result;
         for (int i = r.length - 2; i >= 0; --i) {
